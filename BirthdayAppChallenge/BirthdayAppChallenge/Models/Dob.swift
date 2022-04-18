f626df9dc437e98a69cebbc9bdc84a019564db87
OhReno <92575989+OhReno@users.noreply.github.com>
2022-04-18 17:30:06 +0200

    Fixed UI

--- BirthdayAppChallenge/BirthdayAppChallenge/Models/Dob.swift
+++ BirthdayAppChallenge/BirthdayAppChallenge/Models/Dob.swift
@@ -10,4 +10,17 @@ import Foundation
 struct Dob: Codable {
     let date: String?
     let age: Int?
+    
+    var reformattedDate: String? {
+        get {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd-mm-yyyy"
+            if let newDate = dateFormatter.date(from: date ?? "") {
+                return dateFormatter.string(from: newDate)
+            } else {
+                return nil
+            }
+            
+            }
+    }
 }
