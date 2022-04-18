b342dab8f81c174588533c3ae9c62148a4c7527f
OhReno <92575989+OhReno@users.noreply.github.com>
2022-04-18 16:35:25 +0200

    updated networking

--- BirthdayAppChallenge/BirthdayAppChallenge/Utilities/Networking.swift
+++ BirthdayAppChallenge/BirthdayAppChallenge/Utilities/Networking.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class Networking {
+    static let shared = Networking()
+    
+    func getResults(closure: @escaping (Results?) -> Void) {
+        if let url = URL(string: "https://randomuser.me/api/?results=1000&seed=chalkboard&inc=name,dob") {
+            let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
+                    if let data = data, let decodedResponse = try? JSONDecoder().decode(Results.self, from: data) {
+// Display error
+                    }
+                }
+                if error == nil {
+                    if let data = data, let decodedResponse = try? JSONDecoder().decode(Results.self, from: data) {
+                                                DispatchQueue.main.async {
+                                                    closure(decodedResponse)
+                                                }
+                    }
+                } else {
+                    //Cant find
+                }
+            }
+            task.resume()
+        }
+    }
+    
+}
