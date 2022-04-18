3659768ebf31cfb70bd8ae0bd1ad01e22327a8da
OhReno <92575989+OhReno@users.noreply.github.com>
2022-04-18 15:36:27 +0200

    added outlets to classes

--- BirthdayAppChallenge/BirthdayAppChallenge/Views/UserBirthdayTableViewCell.swift
+++ BirthdayAppChallenge/BirthdayAppChallenge/Views/UserBirthdayTableViewCell.swift
@@ -9,15 +9,21 @@ import UIKit
 
 class UserBirthdayTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var initialsView: UIView!
+    @IBOutlet weak var initialsLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var DOBLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+       
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
