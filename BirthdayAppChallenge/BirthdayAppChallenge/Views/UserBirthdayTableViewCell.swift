6b0380b8c1a34ff7d28aa40252076e3202f9b9cb
OhReno <92575989+OhReno@users.noreply.github.com>
2022-04-18 15:30:34 +0200

    Added tableview and cell view ui

---
+++ BirthdayAppChallenge/BirthdayAppChallenge/Views/UserBirthdayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserBirthdayTableViewCell.swift
+//  BirthdayAppChallenge
+//
+//  Created by Renoy Chowdhury on 18/04/22.
+//
+
+import UIKit
+
+class UserBirthdayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
